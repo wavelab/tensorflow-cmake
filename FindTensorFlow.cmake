45a0cf066fa1288f2aff6b19ea7dc40d60a9b397
Nav Ganti <navganti.12@gmail.com>
2018-02-16 18:42:43 -0500

    fix issue in FindTensorFlow.cmake

--- FindTensorFlow.cmake
+++ FindTensorFlow.cmake
@@ -3,7 +3,7 @@
 include(FindPackageHandleStandardArgs)
 unset(TENSORFLOW_FOUND)
 
-find_path(Tensorflow_INCLUDE_DIR
+find_path(TensorFlow_INCLUDE_DIR
         NAMES
         tensorflow/core
         tensorflow/cc
@@ -12,22 +12,24 @@ find_path(Tensorflow_INCLUDE_DIR
         /usr/local/include/google/tensorflow
         /usr/include/google/tensorflow)
 
-find_library(Tensorflow_LIBRARY
-		NAMES
-		tensorflow_cc
-		tensorflow_framework
+find_library(TensorFlow_CC tensorflow_cc
         HINTS
         /usr/lib
         /usr/local/lib)
 
+find_library(TensorFlow_framework tensorflow_framework
+		HINTS
+		/usr/lib
+		/usr/local/lib)
+
 # set TensorFlow_FOUND
-find_package_handle_standard_args(Tensorflow DEFAULT_MSG Tensorflow_INCLUDE_DIR Tensorflow_LIBRARY)
+find_package_handle_standard_args(TensorFlow DEFAULT_MSG TensorFlow_INCLUDE_DIR TensorFlow_CC TensorFlow_framework)
 
 # set external variables for usage in CMakeLists.txt
 if(TENSORFLOW_FOUND)
-    set(Tensorflow_LIBRARIES ${Tensorflow_LIBRARY})
-    set(Tensorflow_INCLUDE_DIRS ${Tensorflow_INCLUDE_DIR})
+    set(TensorFlow_LIBRARIES ${TensorFlow_CC} ${TensorFlow_framework})
+    set(TensorFlow_INCLUDE_DIRS ${TensorFlow_INCLUDE_DIR})
 endif()
 
 # hide locals from GUI
-mark_as_advanced(Tensorflow_INCLUDE_DIR Tensorflow_LIBRARY)
+mark_as_advanced(TensorFlow_INCLUDE_DIR TensorFlow_CC TensorFlow_framework)
